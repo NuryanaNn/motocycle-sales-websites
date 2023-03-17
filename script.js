1c7dcd41926fd5feef756d92c0893f55266b456f
Nuryana N <nuryan504@gmail.com>
2023-03-17 09:24:37 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper(".mySwiper",{
+    slidesPerView:"auto",
+    spaceBetween:30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delar:2500,
+        disbledonInteraction:false,
+    }
+})
